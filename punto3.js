09151a64512d5f2ca2fea4982f6f79fa083d32f5
victorcarvajal23 <victorcarvajal016@gmail.com>
2021-04-17 15:00:23 -0500

    punto4

--- punto3.js
+++ punto3.js
@@ -1,1 +1,32 @@
 /* 3.	Realizar un programa que pueda sumar dos números leídos por teclado y escribir el resultado. */
+
+//punto 3
+let inicio=true
+let numero1
+let valida=true
+while(inicio==true){
+    if (valida==true)
+    {
+    numero1=parseInt( prompt("ingrese el primer valor"))
+    }   
+valida =Number.isInteger(numero1)
+if (valida==true) {
+    let numero2=parseInt( prompt("ingrese el segundo valor"))
+    let valida2 =Number.isInteger(numero2)
+    if (valida2==true) {
+        console.log("numero uno "+numero1)
+        console.log("numero dos "+numero2)
+        console.log("La suma es "+(numero1+numero2)) 
+        inicio=false
+    }
+    else{
+        alert("ingrese un valor valido")
+        valida=false
+    }  
+}
+else {
+    alert("ingrese un valor valido")
+    inicio=true
+    valida=true
+}
+}
--- punto4.js
+++ punto4.js
@@ -1,1 +1,31 @@
 /* 4.	Realizar un programa de procesos en el que se almacenen 3 números en 3 variables A, B y C.  */
+//punto 4
+
+let valida1=false
+let valida2=false
+let valida3=false
+let numero1,numero2,numero3
+while(valida1==false){
+numero1=parseInt(prompt("ingrese el primer numero"))
+valida1=Number.isInteger(numero1)
+if (valida1==false) {
+    alert("ingrese un valor valido")
+}
+}
+while(valida2==false){
+    numero2=parseInt(prompt("ingrese el segundo numero"))
+    valida2=Number.isInteger(numero2)
+    if (valida2==false) {
+        alert("ingrese un valor valido")
+    }
+}
+ while(valida3==false){
+    numero3=parseInt(prompt("ingrese el tercer numero"))
+    valida3=Number.isInteger(numero3)
+    if (valida3==false) {
+        alert("ingrese un valor valido")
+    }
+}
+console.log("numero uno "+numero1)
+console.log("numero dos "+numero2)
+console.log("numero tres "+numero3)
