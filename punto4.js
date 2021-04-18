95822253f7a67c8db808225702f59cf6137e636e
victorcarvajal23 <victorcarvajal016@gmail.com>
2021-04-18 17:42:23 -0500

    punto7

--- punto4.js
+++ punto4.js
@@ -1,4 +1,5 @@
-/* 4.	Realizar un programa de procesos en el que se almacenen 3 números en 3 variables A, B y C.  */
+/* 4.	Realizar un programa de procesos en el que se almacenen 3 números en 3 variables A, B y C. 
+El diagrama debe decidir cual es el mayor y cual es el menor  */
 //punto 4
 
 let valida1=false
@@ -29,3 +30,27 @@ while(valida2==false){
 console.log("numero uno "+numero1)
 console.log("numero dos "+numero2)
 console.log("numero tres "+numero3)
+
+let vector=[numero1,numero2,numero3]
+let max=vector[0]
+let min=vector[0]
+
+
+for (let i = 0; i < vector.length; i++) {
+    
+    if (vector[i] > max) {
+        max=vector[i]
+    }
+    
+    if (vector[i] < min) {
+        min=vector[i]
+    }
+    
+    
+}
+let max2=Math.max(...vector)
+let min2=Math.min(...vector)
+console.log("numero mayor con funcion "+max2)
+console.log("numero menor con funcion "+min2)
+console.log("numero mayor "+max)
+console.log("numero menor "+min)
--- punto7.js
+++ punto7.js
@@ -3,3 +3,46 @@ el resto de dividir N entre D es 0. Se dice que un número N es perfecto si la s
 de sus divisores (excluido el propio N) es N. Por ejemplo 28 es perfecto, pues sus 
 divisores (excluido elv28) son: 1, 2, 4, 7 y 14 y su suma es 1+2+4+7+14=28. Hacer 
 un organigrama que dado un número N nos diga si es o no perfecto. */
+//punto 7
+
+/* let numeroN=parseInt(prompt("ingrese valor de N"))
+let numeroD=parseInt(prompt("ingrese valor de D"))
+
+let resto=numeroN%numeroD
+
+if (resto==0) {
+    console.log(`D ${numeroD} es dividor de N ${numeroN}`)
+}
+else{
+    console.log(`D ${numeroD} NO es dividor de N ${numeroN}`)
+} */
+let inicio=true
+let suma=0
+let numeroN
+while(inicio==true)
+{
+numeroN=parseInt(prompt("ingrese valor de N"))
+let valida= Number.isInteger(numeroN)
+if (valida==true) {
+    
+    for (let i = 0; i < numeroN; i++) {
+        let divisores=numeroN%i
+        if (divisores==0) {
+        suma=suma+i  
+        }
+    
+    }
+
+    inicio=false
+}
+else{
+    alert("ingrese un valor valido para N")
+    inicio=true
+}
+}
+if (suma==numeroN) {
+    console.log(`N ${numeroN} es un numero perfecto la suma de sus divisores es `+suma)
+}
+else {
+    console.log(`N ${numeroN} NO es un numero perfecto la suma de sus divisores es `+suma)
+}
